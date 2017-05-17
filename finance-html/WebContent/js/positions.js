00464ae4a1ae5831393eb66cd493df92d39255e6
Stephen <github@leonarduk.com>
2017-05-18 00:34:03 +0100

    #29

--- finance-html/WebContent/js/positions.js
+++ finance-html/WebContent/js/positions.js
@@ -7,34 +7,38 @@ var xhr = new XMLHttpRequest();
 
 xhr.onload = function() {// When readystate changes
 	// The following conditional check will not work locally - only on a server
-	//if(xhr.status === 200) {                      // If server status was ok
+	// if(xhr.status === 200) { // If server status was ok
 	responseObject = JSON.parse(xhr.responseText);
 
 	// BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
 	var newContent = '<table border="1"><tr><th>Instrument</th><th>Amount</th></tr>';
 	// Variable to hold HTML
-	for (var i = 0; i < responseObject.holdings.length; i++) {// Loop through object
+	for (var i = 0; i < responseObject.holdings.length; i++) {// Loop through
+																// object
 
 		/*
-		 * amount   24475.88
-		 instrument
-		 name    "CASH"
+		 * amount 24475.88 instrument name "CASH"
 		 */
-		newContent += '<tr><td>' + responseObject.holdings[i].instrument.name + '</td><td>' + responseObject.holdings[i].amount + '</td></tr>';
+		newContent += '<tr><td>' + responseObject.holdings[i].instrument.name
+				+ '</td><td>' + responseObject.holdings[i].amount
+				+ '</td></tr>';
 	}
 
-// Update the page with the new content
-document.getElementById('content').innerHTML = newContent;
+	// Update the page with the new content
+	document.getElementById('content').innerHTML = newContent;
 
-//}
+	// }
 };
 
 xhr.open('GET', 'http://leonarduk.ddns.info:8091/portfolio/api/display', true);
 // Prepare the request
-xhr.send(null); 
+xhr.send(null);
 // Send the request
 
-// When working locally in Firefox, you may see an error saying that the JSON is not well-formed.
-// This is because Firefox is not reading the correct MIME type (and it can safely be ignored).
+// When working locally in Firefox, you may see an error saying that the JSON is
+// not well-formed.
+// This is because Firefox is not reading the correct MIME type (and it can
+// safely be ignored).
 
-// If you get it on a server, you may need to se the MIME type for JSON on the server (application/JSON).
+// If you get it on a server, you may need to se the MIME type for JSON on the
+// server (application/JSON).
--- finance-html/WebContent/js/valuations.js
+++ finance-html/WebContent/js/valuations.js
@@ -12,34 +12,64 @@ xhr.onload = function() {// When readystate changes
 
 		// BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
 		var newContent = '<table border="1"><tr><th>Name</th><th>ISIN</th><th>Code</th><th>Sector</th><th>Type</th>' //
-		+ '<th>Quantity Owned</th>' + '<th>Value Owned</th>' + '<th>Price</th>' + '<th>AsOf</th>' //
-		+ '<th>1D</th>' + '<th>5D</th>' + '<th>21D</th>' + '<th>63D</th>'+ '<th>365D</th>' //
-		+ '<th>SMA (12days)</th>' + '<th>SMA (20days)</th>' + '<th>SMA (50days)</th>' //
-		+ '<th>Global Extrema</th>' + '<th>Moving Momentum</th>' + '</tr>';
+				+ '<th>Quantity Owned</th>'
+				+ '<th>Value Owned</th>'
+				+ '<th>Price</th>'
+				+ '<th>AsOf</th>' //
+				+ '<th>1D</th>'
+				+ '<th>5D</th>'
+				+ '<th>21D</th>'
+				+ '<th>63D</th>'
+				+ '<th>365D</th>' //
+				+ '<th>SMA (12days)</th>'
+				+ '<th>SMA (20days)</th>'
+				+ '<th>SMA (50days)</th>' //
+				+ '<th>Global Extrema</th>'
+				+ '<th>Moving Momentum</th>'
+				+ '</tr>';
 
 		// Variable to hold HTML
 		for (var i = 0; i < responseObject.valuations.length; i++) {//
 			// Loop through object
-			newContent += '<tr><td>' + responseObject.valuations[i].position.instrument.name//
-			+ '</td><td>' + responseObject.valuations[i].position.instrument.isin//
-			+ '</td><td>' + responseObject.valuations[i].position.instrument.code//
-			+ '</td><td>' + responseObject.valuations[i].position.instrument.category//
-			+ '</td><td>' + responseObject.valuations[i].position.instrument.assetType//
-			+ '</td><td>' + responseObject.valuations[i].valuation//
-			+ '</td><td>' + responseObject.valuations[i].position.amount//
-			+ '</td><td>' + responseObject.valuations[i].price//
-			+ '</td><td>' + responseObject.valuations[i].valuationDate//
-			+ '</td><td>' + responseObject.valuations[i].returns.P1D//
-			+ '</td><td>' + responseObject.valuations[i].returns.P5D//
-			+ '</td><td>' + responseObject.valuations[i].returns.P21D//
-			+ '</td><td>' + responseObject.valuations[i].returns.P63D//
-			+ '</td><td>' + responseObject.valuations[i].returns.P365D//
-			+ '</td><td>' + responseObject.valuations[i].recommendation.SMA12days//
-			+ '</td><td>' + responseObject.valuations[i].recommendation.SMA20days//
-			+ '</td><td>' + responseObject.valuations[i].recommendation.SMA50days//
-			+ '</td><td>' + responseObject.valuations[i].recommendation.GlobalExtrema//
-			+ '</td><td>' + responseObject.valuations[i].recommendation.MovingMomentum//
-			+ '</td></tr>';
+			newContent += '<tr><td>'
+					+ responseObject.valuations[i].position.instrument.name//
+					+ '</td><td>'
+					+ responseObject.valuations[i].position.instrument.isin//
+					+ '</td><td>'
+					+ responseObject.valuations[i].position.instrument.code//
+					+ '</td><td>'
+					+ responseObject.valuations[i].position.instrument.category//
+					+ '</td><td>'
+					+ responseObject.valuations[i].position.instrument.assetType//
+					+ '</td><td>'
+					+ responseObject.valuations[i].valuation//
+					+ '</td><td>'
+					+ responseObject.valuations[i].position.amount//
+					+ '</td><td>'
+					+ responseObject.valuations[i].price//
+					+ '</td><td>'
+					+ responseObject.valuations[i].valuationDate//
+					+ '</td><td>'
+					+ responseObject.valuations[i].returns.P1D//
+					+ '</td><td>'
+					+ responseObject.valuations[i].returns.P5D//
+					+ '</td><td>'
+					+ responseObject.valuations[i].returns.P21D//
+					+ '</td><td>'
+					+ responseObject.valuations[i].returns.P63D//
+					+ '</td><td>'
+					+ responseObject.valuations[i].returns.P365D//
+					+ '</td><td>'
+					+ responseObject.valuations[i].recommendation.SMA12days//
+					+ '</td><td>'
+					+ responseObject.valuations[i].recommendation.SMA20days//
+					+ '</td><td>'
+					+ responseObject.valuations[i].recommendation.SMA50days//
+					+ '</td><td>'
+					+ responseObject.valuations[i].recommendation.GlobalExtrema//
+					+ '</td><td>'
+					+ responseObject.valuations[i].recommendation.MovingMomentum//
+					+ '</td></tr>';
 		}
 
 		// Update the page with the new content
@@ -48,14 +78,17 @@ xhr.onload = function() {// When readystate changes
 	}
 };
 
-//xhr.open('GET', 'http://localhost:8091/portfolio/api/report', true);
+// xhr.open('GET', 'http://localhost:8091/portfolio/api/report', true);
 xhr.open('GET', 'http://leonarduk.ddns.info:8091/portfolio/api/report', true);
-//xhr.open('GET', 'valuations.json', true);
+// xhr.open('GET', 'valuations.json', true);
 // Prepare the request
 xhr.send(null);
 // Send the request
 
-// When working locally in Firefox, you may see an error saying that the JSON is not well-formed.
-// This is because Firefox is not reading the correct MIME type (and it can safely be ignored).
+// When working locally in Firefox, you may see an error saying that the JSON is
+// not well-formed.
+// This is because Firefox is not reading the correct MIME type (and it can
+// safely be ignored).
 
-// If you get it on a server, you may need to se the MIME type for JSON on the server (application/JSON).
+// If you get it on a server, you may need to se the MIME type for JSON on the
+// server (application/JSON).
