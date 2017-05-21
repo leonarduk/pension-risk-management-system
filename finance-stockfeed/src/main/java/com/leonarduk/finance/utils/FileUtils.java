a2ac962490123400fa24d2e2850ae7d1e654fffd
Stephen <github@leonarduk.com>
2017-05-21 05:47:14 +0100

    #53

---
+++ finance-stockfeed/src/main/java/com/leonarduk/finance/utils/FileUtils.java
@@ -0,0 +1,36 @@
+package com.leonarduk.finance.utils;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FileUtils {
+	private static final Logger LOGGER = Logger.getLogger(FileUtils.class.getName());
+
+	public static void writeFile(final String fileName, final StringBuilder sb) {
+		/**
+		 * Writing CSV file
+		 */
+		BufferedWriter writer = null;
+		try {
+
+			writer = new BufferedWriter(new FileWriter(fileName));
+			writer.write(sb.toString());
+			FileUtils.LOGGER.info("Saved to " + fileName);
+		}
+		catch (final IOException ioe) {
+			FileUtils.LOGGER.log(Level.SEVERE, "Unable to write CSV file", ioe);
+		}
+		finally {
+			try {
+				if (writer != null) {
+					writer.close();
+				}
+			}
+			catch (final IOException ioe) {
+			}
+		}
+	}
+}
