1ece32aa39ff53be5d36de12cf18894170550712
github@leonarduk.com <github@leonarduk.com>
2017-06-17 00:57:18 +0100

    #63

---
+++ finance-stockfeed/src/test/java/com/leonarduk/finance/analysis/TraderOrderUtilsTest.java
@@ -0,0 +1,31 @@
+package com.leonarduk.finance.analysis;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import eu.verdelhan.ta4j.Order;
+import eu.verdelhan.ta4j.Strategy;
+import eu.verdelhan.ta4j.Tick;
+import eu.verdelhan.ta4j.TimeSeries;
+import eu.verdelhan.ta4j.Trade;
+import jersey.repackaged.com.google.common.collect.Lists;
+
+public class TraderOrderUtilsTest {
+
+	@Test
+	public void testGetOrdersList() {
+		List<Trade> trades = Lists.newLinkedList();
+		trades.add(new Trade(Order.buyAt(0), Order.sellAt(1)));
+		TimeSeries series = new TimeSeries(
+				Lists.newArrayList(new Tick(DateTime.parse("2017-04-01"), 100, 103, 98, 101, 2),
+						new Tick(DateTime.parse("2017-04-02"), 101, 105, 100, 104, 1)));
+		List<Trade> orders = TraderOrderUtils.getOrdersList(trades, series, mock(Strategy.class), "test");
+		assertEquals(1, orders.size());
+	}
+
+}
