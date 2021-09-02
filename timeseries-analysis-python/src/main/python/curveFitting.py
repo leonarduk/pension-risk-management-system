1cde88dadc3e9e7a415de700bb23419b85f5361c
Steve Leonard <github@leonarduk.com>
2021-09-02 14:53:39 +0100

    optimisations

--- timeseries-analysis-python/src/main/python/curveFitting.py
+++ timeseries-analysis-python/src/main/python/curveFitting.py
@@ -3,32 +3,52 @@ from numpy import sin
 from numpy import sqrt
 from numpy import arange
 from pandas import read_csv
+import pandas as pd
+
 from scipy.optimize import curve_fit
 from matplotlib import pyplot
 
-
-# define the true objective function
 def straightlinefit(x, a, b):
     return a * x + b
 
-# define the true objective function
 def sinecurve(x, a, b, c, d):
     return a * sin(b - x) + c * x ** 2 + d
 
-# define the true objective function
 def curvedlinefit(x, a, b, c):
 	return a * x + b * x**2 + c
 
 # load the dataset
 def loadData():
     url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/longley.csv'
+    # 1     2           3       4       5           6       7
+    # 83,   234.289,    235.6,  159,    107.608,    1947,   60.323
     dataframe = read_csv(url, header=None)
     return dataframe.values
 
+def getDataFrame(ticker, years=10 ):
+    CSV_URL = 'http://localhost:8091/stock/download/ticker/' + ticker + '?years=' + str(years) + '&interpolate=true&clean=true'
+
+    # df = pd.read_csv(CSV_URL, usecols=['date', 'close'], index_col='date')
+    df = read_csv(CSV_URL, header=0 )
+
+    # df['date'].str.replace("-", "").astype(int)
+    df["date"] = pd.to_datetime(df["date"]).dt.strftime("%Y%m%d").astype(int)
+    # df['date'].dt.strftime("%Y-%m-%d").astype(int)
+    df.insert(0, 'ID', range(1, 1 + len(df)))
+    print(CSV_URL + " " + str(df.size))
+
+    return df
+
 def plotdata(fittingmethod):
     # choose the input and output variables
-    data = loadData()
-    x, y = data[:, 4], data[:, -1]
+    # data = loadData()
+
+    #   1           2       3       4       5       6       7
+    # date,         open,   high, low,      close, volume, comment
+    # 2020-09-02,   15.15, 15.15, 15.14,    15.15, 85.00, Alphavantage
+    data = getDataFrame("HMBR.L",1 )
+    y = data["close"]
+    x = data["ID"]
     # curve fit
     popt, _ = curve_fit(fittingmethod, x, y)
     # summarize the parameter values
@@ -49,6 +69,6 @@ def plotdata(fittingmethod):
     pyplot.plot(x_line, y_line, '--', color='red')
     pyplot.show()
 
-# plotdata(sinecurve)
+plotdata(sinecurve)
 # plotdata(straightlinefit)
-plotdata(curvedlinefit)
+# plotdata(curvedlinefit)
--- timeseries-analysis-python/src/main/python/datacollection/downloadData.py
+++ timeseries-analysis-python/src/main/python/datacollection/downloadData.py
@@ -19,35 +19,35 @@ def getDataFrame(ticker, years=10 ):
 
     return newdf
 
-
-# compile the list of dataframes you want to merge
-data_frames = [getDataFrame("REL.L"), getDataFrame("AZN.L"), getDataFrame("CTY.L"),
+def main():
+    # compile the list of dataframes you want to merge
+    data_frames = [getDataFrame("REL.L"), getDataFrame("AZN.L"), getDataFrame("CTY.L"),
                getDataFrame("PHGP.L"), getDataFrame("RDSA.L"), getDataFrame("IMB.L"),
                getDataFrame("ULVR.L"), getDataFrame("GSK.L"), getDataFrame("DGE.L"),
                getDataFrame("EXPN.L"), getDataFrame("LGEN.L"), getDataFrame("GRG.L"),
                getDataFrame("ISXF.L"), getDataFrame("JRS.L"), getDataFrame("XMCX.L"),
               getDataFrame("IGUS.L"), getDataFrame("CGT.L"), getDataFrame("GILI.L"), getDataFrame("XLGS.L"), getDataFrame("WATL.L"), getDataFrame("XESW.L")]
 
-df = reduce(lambda  left,right: pd.merge(left,right,on=['date'], how='outer'), data_frames)
+    df = reduce(lambda  left,right: pd.merge(left,right,on=['date'], how='outer'), data_frames)
 
-# Calculate expected returns and sample covariance
-mu = expected_returns.mean_historical_return(df)
-S = risk_models.sample_cov(df)
+    # Calculate expected returns and sample covariance
+    mu = expected_returns.mean_historical_return(df)
+    S = risk_models.sample_cov(df)
 
-# Optimize for maximal Sharpe ratio
-ef = EfficientFrontier(mu, S)
-raw_weights = ef.max_sharpe()
-cleaned_weights = ef.clean_weights()
-ef.save_weights_to_file("weights.csv")  # saves to file
-print(cleaned_weights)
-ef.portfolio_performance(verbose=True)
+    # Optimize for maximal Sharpe ratio
+    ef = EfficientFrontier(mu, S)
+    raw_weights = ef.max_sharpe()
+    cleaned_weights = ef.clean_weights()
+    ef.save_weights_to_file("weights.csv")  # saves to file
+    print(cleaned_weights)
+    ef.portfolio_performance(verbose=True)
 
-from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
+    from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
 
-latest_prices = get_latest_prices(df)
+    latest_prices = get_latest_prices(df)
 
-da = DiscreteAllocation(cleaned_weights, latest_prices, total_portfolio_value=400000)
-allocation, leftover = da.greedy_portfolio()
-print("Discrete allocation:", allocation)
-print("Funds remaining: ${:.2f}".format(leftover))
+    da = DiscreteAllocation(cleaned_weights, latest_prices, total_portfolio_value=400000)
+    allocation, leftover = da.greedy_portfolio()
+    print("Discrete allocation:", allocation)
+    print("Funds remaining: ${:.2f}".format(leftover))
 
