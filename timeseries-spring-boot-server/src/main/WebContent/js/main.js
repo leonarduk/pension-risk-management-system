d5ede12951d0fe893c5e90794c8bc10558cb030d
Steve Leonard <github@leonarduk.com>
2022-08-03 22:24:23 +0100

    remove spring boot as using lambda

--- timeseries-spring-boot-server/src/main/WebContent/js/main.js
+++
@@ -1,5 +0,0 @@
-Date.prototype.toDateInputValue = (function() {
-	var local = new Date(this);
-	local.setMinutes(this.getMinutes() - this.getTimezoneOffset())
-	return local.toJSON().slice(0,10);
-}
--- timeseries-spring-boot-server/src/main/java/com/leonarduk/finance/api/InstrumentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leonarduk.finance.api;
-
-import com.leonarduk.finance.stockfeed.Instrument;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InstrumentRepository extends JpaRepository<Instrument, String> {
-}
