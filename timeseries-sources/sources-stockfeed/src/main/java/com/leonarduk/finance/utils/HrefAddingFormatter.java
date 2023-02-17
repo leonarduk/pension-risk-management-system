3af7fdaee8afb59882954aa3bccda5db18dd9b7f
Steve Leonard <github@leonarduk.com>
2023-02-17 23:26:29 +0000

    Fix build

---
+++ timeseries-sources/sources-stockfeed/src/main/java/com/leonarduk/finance/utils/HrefAddingFormatter.java
@@ -0,0 +1,19 @@
+package com.leonarduk.finance.utils;
+
+public class HrefAddingFormatter implements ValueFormatter {
+    final private String ticker;
+    final private int years;
+
+    public HrefAddingFormatter(final String ticker, final int years) {
+        this.years = years;
+        this.ticker = ticker;
+    }
+
+    @Override
+    public String format(final Object value) {
+        return new StringBuilder("<a href=\"/stock/display?ticker=")
+                .append(this.ticker).append("&years=").append(this.years)
+                .append("\">").append(value).append("</a>").toString();
+    }
+
+}
---
+++ timeseries-sources/sources-stockfeed/src/test/java/com/leonarduk/finance/stockfeed/feed/ft/FTFeedTest.java
@@ -0,0 +1,44 @@
+package com.leonarduk.finance.stockfeed.feed.ft;
+
+import com.leonarduk.finance.stockfeed.Instrument;
+import com.leonarduk.finance.stockfeed.feed.yahoofinance.StockV1;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class FTFeedTest {
+
+    FTFeed feed;
+
+    @Before
+    public void setUp() throws Exception {
+        feed = new FTFeed();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void get() throws IOException {
+        Optional<StockV1> result = feed.get(Instrument.fromString("PHGP"), 1, false);
+        if (result.isPresent()) {
+            System.out.println(result.get().getHistory());
+        }
+    }
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void getSource() {
+    }
+
+    @Test
+    public void isAvailable() {
+    }
+}
