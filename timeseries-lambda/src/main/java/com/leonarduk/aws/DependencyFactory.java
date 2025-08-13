fa1be082f19f91a78038d3a72810959a9f5e55cf
Steve <leonarduk@users.noreply.github.com>
2025-08-13 21:05:35 +0100

    Test singleton behavior of DependencyFactory

--- timeseries-lambda/src/main/java/com/leonarduk/aws/DependencyFactory.java
+++ timeseries-lambda/src/main/java/com/leonarduk/aws/DependencyFactory.java
@@ -9,6 +9,7 @@ public class DependencyFactory {
     private DependencyFactory() {
     }
 
+    private static volatile StockFeed instance;
 
     /**
      * Return an instance of a stock feed. This is a singleton, so subsequent calls will return the same instance.
@@ -16,7 +17,18 @@ public class DependencyFactory {
      * @return A stock feed
      */
     public static StockFeed stockFeed() {
-        return new IntelligentStockFeed(new S3DataStore("timeseries-leonarduk",
-                "timeseries", Region.EU_WEST_1.toString()));
+        if (instance == null) {
+            synchronized (DependencyFactory.class) {
+                if (instance == null) {
+                    instance =
+                            new IntelligentStockFeed(
+                                    new S3DataStore(
+                                            "timeseries-leonarduk",
+                                            "timeseries",
+                                            Region.EU_WEST_1.toString()));
+                }
+            }
+        }
+        return instance;
     }
 }
