e84b12ed822db471263d4846b6f475fec74bdc20
Steve <leonarduk@users.noreply.github.com>
2025-08-14 23:14:36 +0100

    Add serialVersionUID to AlphaVantageException

--- alphavantage4j/src/main/java/org/patriques/output/AlphaVantageException.java
+++ alphavantage4j/src/main/java/org/patriques/output/AlphaVantageException.java
@@ -1,6 +1,12 @@
 package org.patriques.output;
 
+/**
+ * Exception thrown when an error occurs while parsing or retrieving data from the
+ * Alpha Vantage service.
+ */
 public class AlphaVantageException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
   public AlphaVantageException(String message, Exception e) {
     super(message, e);
   }
