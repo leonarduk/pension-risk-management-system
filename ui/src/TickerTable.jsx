47306c6a43d204d3800cec4bee06caaeaf1976a9
Steve <leonarduk@users.noreply.github.com>
2025-08-14 07:55:47 +0100

    test: add view mode toggle regression test

--- ui/src/TickerTable.jsx
+++ ui/src/TickerTable.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 
-export default function TickerTable({ data }) {
+export default function TickerTable({ data, viewMode = 'total' }) {
+  const total = Object.values(data).reduce((sum, prices) => {
+    const dates = Object.keys(prices);
+    const latestDate = dates[dates.length - 1];
+    const latestPrice = prices[latestDate];
+    return sum + latestPrice;
+  }, 0);
+
   const renderRows = () =>
     Object.entries(data).map(([ticker, prices]) => {
       const dates = Object.keys(prices);
       const latestDate = dates[dates.length - 1];
       const latestPrice = prices[latestDate];
+      const value =
+        viewMode === 'relative' && total > 0
+          ? (latestPrice / total).toFixed(2)
+          : latestPrice;
       return (
         <tr key={ticker}>
           <td>{ticker}</td>
-          <td>{latestPrice}</td>
+          <td>{value}</td>
         </tr>
       );
     });
