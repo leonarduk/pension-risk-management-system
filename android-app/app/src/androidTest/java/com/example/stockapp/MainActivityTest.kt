1578d1bdd5824eef2c0deb68ffd698be9ec633e0
Steve <leonarduk@users.noreply.github.com>
2025-08-16 07:58:31 +0100

    Add unit and UI tests for Android app

---
+++ android-app/app/src/androidTest/java/com/example/stockapp/MainActivityTest.kt
@@ -0,0 +1,23 @@
+package com.example.stockapp
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun listView_isDisplayed() {
+        onView(withId(R.id.listView)).check(matches(isDisplayed()))
+    }
+}
