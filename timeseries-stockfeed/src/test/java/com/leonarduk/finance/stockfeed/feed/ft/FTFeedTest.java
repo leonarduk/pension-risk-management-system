56d94d4016ed946c0d33e0a633146a4e3b33724a
Steve <leonarduk@users.noreply.github.com>
2025-08-17 12:09:23 +0100

    refactor: standardize logging

--- timeseries-stockfeed/src/test/java/com/leonarduk/finance/stockfeed/feed/ft/FTFeedTest.java
+++ timeseries-stockfeed/src/test/java/com/leonarduk/finance/stockfeed/feed/ft/FTFeedTest.java
@@ -2,6 +2,7 @@ package com.leonarduk.finance.stockfeed.feed.ft;
 
 import com.leonarduk.finance.stockfeed.Instrument;
 import com.leonarduk.finance.stockfeed.feed.yahoofinance.StockV1;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.Optional;
 
+@Slf4j
 public class FTFeedTest {
 
     FTFeed feed;
@@ -23,7 +25,7 @@ public class FTFeedTest {
     public void get() throws IOException {
         Optional<StockV1> result = feed.get(Instrument.fromString("PHGP"), 1, false);
         if (result.isPresent()) {
-            System.out.println(result.get().getHistory());
+            log.info("{}", result.get().getHistory());
         }
     }
 }
